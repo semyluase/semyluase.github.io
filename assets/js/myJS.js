fbd38d5a93f4a9f3c8d977beb048a27851b1965d
Semy Luase <semyvaldes12@gmail.com>
2021-02-27 19:36:59 +0700

    commit

---
+++ assets/js/myJS.js
@@ -0,0 +1,14 @@
+let typed2 = new Typed('.works', {
+    strings: ['<i>Web Developer</i>', '<i>PegaSystem Developer</i>', '<i>BackEnd Developer</i>'],
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true,
+    showCursor: true,
+    smartBackspace: true
+});
+
+const myChart = $(".php").donutty({
+    min: -100,
+    max: 100,
+    value: 50
+});
