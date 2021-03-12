0380466bfacb3779cfa9a15340930df72812a0ee
Semy Luase <semyvaldes12@gmail.com>
2021-03-12 15:04:55 +0700

    menambahkkan script untuk form to google sheet

--- assets/js/myJS.js
+++ assets/js/myJS.js
@@ -7,8 +7,39 @@ let typed2 = new Typed('.works', {
     smartBackspace: true
 });
 
-const myChart = $(".php").donutty({
-    min: -100,
-    max: 100,
-    value: 50
-});
+const scriptURL = 'https://script.google.com/macros/s/AKfycby17WjamfnRYqxPIqn61cZLif6C1lLKP_yRyMstvYEWY9GERQDrCGUYselaG5AIBeYm/exec';
+const form = document.forms['semy-site-contact-form'];
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    btnKirim.classList.add('d-none');
+    btnLoading.classList.remove('d-none');
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => {
+            console.log('Success!', response);
+            btnKirim.classList.remove('d-none');
+            btnLoading.classList.add('d-none');
+            Swal.fire({
+                icon: 'success',
+                title: 'Berhasil',
+                html: '<b>Terima Kasih! </b>Pesan sudah terkirim'
+            });
+            form.reset();
+        })
+        .catch(error => {
+            console.error('Error!', error.message);
+            btnKirim.classList.remove('d-none');
+            btnLoading.classList.add('d-none');
+            Swal.fire({
+                icon: 'error',
+                title: 'Ooops',
+                html: '<b>Mohon Maaf! </b>Pesan gagal terkirim'
+            });
+        })
+})
